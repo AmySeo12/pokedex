64cdb8448c61b12602206201d80eab22276c788d
MaryoriQuiroz <maryori.quiroz@codigoti.tech>
2019-09-20 19:10:34 -0500

    Iniciando el repositorio de pokedex, subiendo los primeros archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PokedexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
