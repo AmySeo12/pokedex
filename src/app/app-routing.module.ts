12b989e43ef6de6ee6d36c8e7db2cf0234a12a35
MaryoriQuiroz <maryori.quiroz@codigoti.tech>
2019-09-21 21:18:09 -0500

    Se sube el backend falso que se usará para el inicio de sesión y el registrar una nueva cuenta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { PokedexComponent } from './pokedex/pokedex.component';
+import { RegistrerComponent } from './registrer/registrer.component';
+import { AuthGuard } from './_guards';
+const routes: Routes = [
+  { path: '', component: PokedexComponent, canActivate: [AuthGuard] },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegistrerComponent },
+  
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PokedexComponent } from './pokedex/pokedex.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
+import { RegistrerComponent } from './registrer/registrer.component';
+
+import { fakeBackendProvider } from './_helpers';
+
+
+import { AlertComponent } from './_components/alert.component';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PokedexComponent
+    PokedexComponent,
+    RegistrerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    fakeBackendProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
